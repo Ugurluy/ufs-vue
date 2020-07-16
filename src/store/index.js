5959541e6f035f1d5b3d5610ddae31b131f7fae8
Yagiz Ugurlu <yagiz.ugurlu02@bilgiedu.net>
2020-07-16 23:10:44 +0300

    Insert Hook and Loop Form

--- src/store/index.js
+++ src/store/index.js
@@ -1,119 +1,133 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 Vue.prototype.$http = axios;
 
 axios.interceptors.request.use(
   (config) => {
-    var token = localStorage.getItem('jwt')
-    if(token){
-      config.headers['Authorization'] = 'Bearer ' + token
+    var token = localStorage.getItem("jwt");
+    if (token) {
+      config.headers["Authorization"] = "Bearer " + token;
     }
-    config.headers['Content-Type'] = 'application/json'
-    return config
+    config.headers["Content-Type"] = "application/json";
+    return config;
   },
-  (error)=> {
-    return Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
   }
-)
+);
 
 // const BASE_URL = 'http://localhost:8080/api/';
 
 export default new Vuex.Store({
   state: {
     user: null,
-    dikisForm:{},
-    hookandLoopForm:[]
+    dikisForm: {},
+    hookandLoopForm: [],
+    insertHookandLoopForm: {},
   },
   mutations: {
-    LOGIN_USER(state, data){
-      state.user = data
-      localStorage.setItem('jwt', data.jwt)         // TODO: jwt token is not present in the localStorage once its manually deleted
+    LOGIN_USER(state, data) {
+      state.user = data;
+      localStorage.setItem("jwt", data.jwt); // TODO: jwt token is not present in the localStorage once its manually deleted
     },
 
-    LOGOUT_USER(){
-      localStorage.removeItem('jwt')
-      location.replace('/login')
-      location.reload()
+    LOGOUT_USER() {
+      localStorage.removeItem("jwt");
+      location.replace("/login");
+      location.reload();
     },
 
-    SIGNUP_USER(state, data){
+    SIGNUP_USER(state, data) {
       state.user = data;
     },
 
-    GET_DIKIS_FORM(state, data){
-      state.dikisForm = data
+    GET_DIKIS_FORM(state, data) {
+      state.dikisForm = data;
     },
 
-    GET_HOOK_LOOP_FORM(state, data){
-      state.hookandLoopForm = data
-    }
-    
+    GET_HOOK_LOOP_FORM(state, data) {
+      state.hookandLoopForm = data;
+    },
+
+    INSERT_HOOK_LOOP(state, data) {
+      state.insertHookandLoopForm = data.formData;
+    },
   },
   actions: {
-    login({commit}, credentials){
+    login({ commit }, credentials) {
       return axios
-      .post('http://localhost:8080/api/auth/signin', credentials)
-      .catch(error => console.log(error))
-      .then(({data}) => {
-        if(!data.success){
-          console.log(data.errorDescription);
-        }else{
-          commit('LOGIN_USER', data)
-          
-        }
-      })
+        .post("http://localhost:8080/api/auth/signin", credentials)
+        .catch((error) => console.log(error))
+        .then(({ data }) => {
+          if (!data.success) {
+            console.log(data.errorDescription);
+          } else {
+            commit("LOGIN_USER", data);
+          }
+        });
     },
 
-    logout({commit}){
+    logout({ commit }) {
       return axios
-      .delete('http://localhost:8080/api/auth/signin')
-      .catch(error => console.log(error))
-      .then(({data}) => {
-        commit('LOGOUT_USER', data)
-      })
+        .delete("http://localhost:8080/api/auth/signin")
+        .catch((error) => console.log(error))
+        .then(({ data }) => {
+          commit("LOGOUT_USER", data);
+        });
     },
 
-    signup({commit}, credentials){
+    signup({ commit }, credentials) {
       return axios
-      .post('http://localhost:8080/api/auth/signup', credentials)
-      .catch(error => console.log(error))
-      .then(({data}) => {
-        if(!data.success){
-          console.log(data.errorDescription);
-        }else{
-          commit('SIGNUP_USER', data)
-        }
-      })
+        .post("http://localhost:8080/api/auth/signup", credentials)
+        .catch((error) => console.log(error))
+        .then(({ data }) => {
+          if (!data.success) {
+            console.log(data.errorDescription);
+          } else {
+            commit("SIGNUP_USER", data);
+          }
+        });
     },
 
-    getDikisForm({commit}){
+    getDikisForm({ commit }) {
       return axios
-      .get('http://localhost:8080/api/getDikisGunlukImalatRaporu/dikisGunlukImalatRaporu')
-      .catch(error => console.log(error))
-      .then(({data}) => {
-        commit('GET_DIKIS_FORM', data)
-      })
+        .get(
+          "http://localhost:8080/api/getDikisGunlukImalatRaporu/dikisGunlukImalatRaporu"
+        )
+        .catch((error) => console.log(error))
+        .then(({ data }) => {
+          commit("GET_DIKIS_FORM", data);
+        });
     },
 
-    getHookandLoopForm({commit}){
+    getHookandLoopForm({ commit }) {
       return axios
-      .get('http://localhost:8080/api/gethookAndLoopGunlukImalat/HookAndLoopGunlukImalat')
-      .catch(error => console.log(error))
-      .then(({data}) => {
-        commit('GET_HOOK_LOOP_FORM', data)
-      })
-    }
+        .get(
+          "http://localhost:8080/api/gethookAndLoopGunlukImalat/HookAndLoopGunlukImalat"
+        )
+        .catch((error) => console.log(error))
+        .then(({ data }) => {
+          commit("GET_HOOK_LOOP_FORM", data);
+        });
+    },
+
+    insertHookAndLoopGunlukImalat({ commit }, formData) {
+      return axios
+        .post("http://localhost:8080/api/inserthookAndLoopGunlukImalat", formData)
+        .catch((error) => console.log(error))
+        .then(({ data }) => {
+          commit("INSERT_HOOK_LOOP", data);
+        });
+    },
   },
   getters: {
-    loggedIn(state){
-      return !!state.user
-    }
+    loggedIn(state) {
+      return !!state.user;
+    },
   },
 
-  modules: {
-
-  }
-})
+  modules: {},
+});
--- src/views/forms/HookandLoop.vue
+++ src/views/forms/HookandLoop.vue
@@ -1,15 +1,8 @@
 <template>
-    <v-row justify="center">
+  <v-row justify="center">
     <v-dialog v-model="dialog" persistent max-width="600px">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="indigo darken-4"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          Yeni Üretim Ekle
-        </v-btn>
+        <v-btn color="indigo darken-4" dark v-bind="attrs" v-on="on">Yeni Üretim Ekle</v-btn>
       </template>
       <v-card>
         <v-card-title>
@@ -19,11 +12,14 @@
           <v-container>
             <v-row>
               <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="formHookandLoop.recordId" label="Record ID" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
                 <v-text-field v-model="editedItem.isEmri" label="Terapi No" required></v-text-field>
               </v-col>
               <v-col cols="12" sm="6">
                 <v-select
-                  v-model="editedItem.musteri"  
+                  v-model="editedItem.musteri"
                   :items="['Mirka', 'Car System', 'Fiat', 'SDK', 'Mercedes', 'Renault', 'Buff and Shine', 'Gelson', 'Marvel']"
                   label="Müşteri"
                   required
@@ -38,8 +34,7 @@
                 ></v-select>
               </v-col>
               <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                v-model="editedItem.spAdet"  label="Sipariş Adet" required></v-text-field>
+                <v-text-field v-model="editedItem.spAdet" label="Sipariş Adet" required></v-text-field>
               </v-col>
               <v-col cols="12" sm="6">
                 <v-select
@@ -61,11 +56,7 @@
                 <v-text-field v-model="editedItem.deriAdet" label="Deri Adet" required></v-text-field>
               </v-col>
               <v-col cols="12" sm="6">
-                <v-select
-                  :items="['İnce', 'Kalın', 'Dijital']"
-                  label="Cırt Türü"
-                  required
-                ></v-select>
+                <v-select :items="['İnce', 'Kalın', 'Dijital']" label="Cırt Türü" required></v-select>
               </v-col>
               <v-col cols="12" sm="6">
                 <v-select
@@ -75,18 +66,10 @@
                 ></v-select>
               </v-col>
               <v-col cols="12" sm="6">
-                <v-select
-                  :items="['Süngersiz','3', '5']"
-                  label="Sünger (mm)"
-                  required
-                ></v-select>
+                <v-select :items="['Süngersiz','3', '5']" label="Sünger (mm)" required></v-select>
               </v-col>
               <v-col cols="12" sm="6">
-                <v-select
-                  :items="['Soft', 'Hard', 'CRT']"
-                  label="Taban"
-                  required
-                ></v-select>
+                <v-select :items="['Soft', 'Hard', 'CRT']" label="Taban" required></v-select>
               </v-col>
               <v-col cols="12">
                 <v-text-field label="Kalıp Adet" required></v-text-field>
@@ -98,93 +81,121 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="blue darken-1" text @click="dialog = false">Kapat</v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false">Kaydet</v-btn>
+          <v-btn color="blue darken-1" text @click="insertHookAndLoopGunlukImalat">Kaydet</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
     <v-container>
-        <v-card>
+      <v-card>
         <v-card-title>
           <span class="headline">Hook and Loop Günlük İmalat Formu</span>
         </v-card-title>
         <v-spacer></v-spacer>
         <v-data-table :headers="headers" :items="hookandLoopForm" hide-actions class="elevation-1">
-            <template slot="items" >
-                <td>{{ hookandLoopForm.isEmri }}</td>
-                
-                <td class="text-xs-right">{{ hookandLoopForm.musteri }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.cap }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.spAdet }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.tuyYuksekligi }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.deriTuru }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.deriAdet }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.cirtTuru }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.cirtRengi }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.sunger }}</td>
-                <td class="text-xs-right">{{ hookandLoopForm.taban }}</td>
-                
-                <td class="justify-center layout px-0">
-                <v-btn icon class="mx-0" @click="editItem(hookandLoopForm)">
+          <template slot="items">
+            <td>{{ hookandLoopForm.isEmri }}</td>
+
+            <td class="text-xs-right">{{ hookandLoopForm.musteri }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.cap }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.spAdet }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.tuyYuksekligi }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.deriTuru }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.deriAdet }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.cirtTuru }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.cirtRengi }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.sunger }}</td>
+            <td class="text-xs-right">{{ hookandLoopForm.taban }}</td>
+
+            <td class="justify-center layout px-0">
+              <v-btn icon class="mx-0" @click="editItem(hookandLoopForm)">
                 <v-icon color="teal">edit</v-icon>
-                </v-btn>
-                <v-btn icon class="mx-0" @click="deleteItem(hookandLoopForm)">
+              </v-btn>
+              <v-btn icon class="mx-0" @click="deleteItem(hookandLoopForm)">
                 <v-icon color="pink">delete</v-icon>
-                </v-btn>
-                </td>
-            </template>
+              </v-btn>
+            </td>
+          </template>
         </v-data-table>
-        </v-card>
-        
+      </v-card>
     </v-container>
   </v-row>
-  
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
+      formHookandLoop:{
+        recordId: 0,
+        isEmri: 0,
+        musteri: "",
+        cap: 0,
+        spAdet: 0,
+        tuyYuksekligi: 0,
+        deriTuru: "",
+        deriAdet: 0,
+        cirtTuru: "",
+        cirtRengi: "",
+        sunger: "",
+        taban: "",
+      },
       dialog: false,
       headers: [
-          { text: 'Terapi No', value: 'terapiNo',sortable: false },
-          { text: 'Müşteri', value: 'musteri',sortable: false },
-          { text: 'Çap', value: 'cap' ,sortable: false},
-          { text: 'Sipariş Adet', value: 'spAdet',sortable: false },
-          { text: 'Tüy Yüksekliği', value: 'tuyYuks',sortable: false },
-          { text: 'Deri Türü', value: 'deriTuru' ,sortable: false},
-          { text: 'Deri Adet', value: 'deriAdet' ,sortable: false},
-          { text: 'Cırt Türü', value: 'cırtTuru',sortable: false },
-          { text: 'Cırt Rengi', value: 'cırtRengi',sortable: false },
-          { text: 'Sünger', value: 'sunger',sortable: false },
-          { text: 'Taban', value: 'taban',sortable: false }
+        { text: "Terapi No", value: "isEmri", sortable: false },
+        { text: "Müşteri", value: "musteri", sortable: false },
+        { text: "Çap", value: "cap", sortable: false },
+        { text: "Sipariş Adet", value: "spAdet", sortable: false },
+        { text: "Tüy Yüksekliği", value: "tuyYuksekligi", sortable: false },
+        { text: "Deri Türü", value: "deriTuru", sortable: false },
+        { text: "Deri Adet", value: "deriAdet", sortable: false },
+        { text: "Cırt Türü", value: "cirtTuru", sortable: false },
+        { text: "Cırt Rengi", value: "cirtRengi", sortable: false },
+        { text: "Sünger", value: "sunger", sortable: false },
+        { text: "Taban", value: "taban", sortable: false }
+      ],
 
-          
-        ],
       editedItem: {
-            isEmri: 0,
-            musteri: '',            
-            cap: 0,
-            spAdet: 0,
-            tuyYuksekligi: 0,
-            deriTuru: '',
-            deriAdet: 0,
-            cirtTuru: 0,
-            cirtRengi: '',
-            sunger: 0,
-            taban: ''
-        }
-    }  
-    },
-   mounted(){
-        this.$store.dispatch('getHookandLoopForm')
-    },
-    computed: {
-        hookandLoopForm(){
-            return this.$store.state.hookandLoopForm
-        }
-    },
-    initialize() {
-
-    }   
-}
+        isEmri: 0,
+        musteri: "",
+        cap: 0,
+        spAdet: 0,
+        tuyYuksekligi: 0,
+        deriTuru: "",
+        deriAdet: 0,
+        cirtTuru: 0,
+        cirtRengi: "",
+        sunger: 0,
+        taban: ""
+      }
+    };
+  },
+  mounted() {
+    this.$store.dispatch("getHookandLoopForm");
+  },
+  computed: {
+    hookandLoopForm() {
+      return this.$store.state.hookandLoopForm;
+    }
+  },
+  initialize() {},
+  methods: {
+    insertHookAndLoopGunlukImalat() {
+      var formData = {
+        recordId: this.formHookandLoop.recordId,
+        isEmri: this.formHookandLoop.isEmri,
+        musteri: this.formHookandLoop.musteri,
+        cap: this.formHookandLoop.cap,
+        spAdet: this.formHookandLoop.spAdet,
+        tuyYuksekligi: this.formHookandLoop.tuyYuksekligi,
+        deriTuru: this.formHookandLoop.deriTuru,
+        deriAdet: this.formHookandLoop.deriAdet,
+        cirtTuru: this.formHookandLoop.cirtTuru,
+        cirtRengi: this.formHookandLoop.cirtRengi,
+        sunger: this.formHookandLoop.sunger,
+        taban: this.formHookandLoop.taban
+      };
+      this.$store.dispatch("insertHookAndLoopGunlukImalat", formData);
+    }
+  }
+};
 </script>
